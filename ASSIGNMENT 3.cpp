a59d2279f93aa0ce80bec2b73eada1f0b10d7ff2
information2020 <afrienduned4real@gmail.com>
2015-05-22 08:36:38 +0100

    C++ Assignment
    programme source code.

---
+++ ASSIGNMENT 3.cpp
@@ -0,0 +1,26 @@
+/*
+* Question3.cpp
+*
+*	Name:			[OJONG KINGSLEY MBIA]
+*	Department:		[MATHEMATICS]
+*	Jamb Reg No:	[47720111CH]
+*
+* 	Program that prints a box, an oval, an arrow and a diamond.
+*
+*	Stub file to enable me complete assignment #1 - question #3
+*/
+
+# include <iostream>
+main ()
+{
+	std::cout<<"********         ***                *               *       \n";
+	std::cout<<"*      *       *     *	           ***            *   *   \n";
+	std::cout<<"*      *      *       *	          *****          *     * \n";  
+	std::cout<<"*      *      *       *	            *           *       *\n";
+	std::cout<<"*      *      *       *             *          *         *\n";
+	std::cout<<"*      *      *       *             *           *       *\n";
+	std::cout<<"*      *      *       *             *            *     *\n";
+	std::cout<<"*      *       *     *              *             *   *  \n";
+	std::cout<<"********         ***                *               *  \n";
+	return 0;
+	}
---
+++ ASSIGNMENt 2.cpp
@@ -0,0 +1,28 @@
+/*
+* Question2.cpp
+*
+*	Name:			[OJONG KINGSLEY MBIA]
+*	Department:		[MATHEMATICS]
+*	Jamb Reg No:	[47720111CH]
+*
+* 	Program that reads two integers and determines
+*	if first is a multiple of second.
+*
+*	Stub file to enable me complete assignment #1 - question #2
+*/
+//
+# include <iostream>
+int main()
+{
+	int j, k;
+	std:: cout <<"Enter an integer\j";
+	std:: cin >> j;
+	std:: cout <<"Enter an integer\k";
+	std:: cin >> k;
+	if (j%k==0)
+	std:: cout << j <<" is a Multiple of" << k;
+	else
+	std:: cout << j << " is Not a multiple of" << k;
+	return 0;
+}
+
